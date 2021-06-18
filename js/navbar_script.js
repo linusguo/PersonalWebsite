3cb02a4e1d6b043ef38766dc35c80a93164728c5
linusguo <guo.yus@northeastern.edu>
2021-06-18 13:42:24 -0700

    Implemented responsive navbar

--- js/navbar_script.js
+++ js/navbar_script.js
@@ -1,10 +1,19 @@
 $(document).ready(function () {
-  $("a.navbar").hover(function() {
+  // Highlight the link when the mouse hovers on
+  $("a.navbar_large").hover(function() {
       $(this).css("background-color", "white");
       $(this).css("color", "black");
-    }, function() {
+    },
+    // Remove highlight when the mouse moves away
+    function() {
       $(this).css("background-color", "black");
       $(this).css("color", "white");
     }
   );
+  // Call the vertical navbar out when clicking on the menu button
+  $("a.mobile").click(function (e) {
+    e.preventDefault();
+      // Click the menu button to toggle the links' class name
+    $("a.navbar_large").toggleClass("responsive");
+  });
 });
--- js/readmore_script.js
+++ js/readmore_script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // Highlight the link when the mouse hovers on
   $("a.link").hover(function() {
       $(this).css("background-color", "white");
       $(this).css("color", "black");
